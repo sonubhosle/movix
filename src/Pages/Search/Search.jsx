8794491c7433f2dc181491c27773f86e7fae8758
sonubhosle <sbhosle1011@gmail.com>
2023-10-06 00:05:42 +0530

    update

--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-import { fetchDataFromApi } from "../../utils/api";
+import { fetchDataFromApi } from "../../utils/Api";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 import MovieCard from "../../components/movieCard/MovieCard";
 import Spinner from "../../components/spinner/Spinner";
